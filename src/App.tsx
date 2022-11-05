5cc48bd07ef36cd53d689ab0b4ccdcd9429650e2
Gabriel Gaede <gabriel.gaede@troika.ind.br>
2022-11-05 17:14:42 -0300

    create transaction context

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
-
+import { TransactionsProvider } from './TransactionsContext';
 import { GlobalStyle } from "./styles/global";
+
 
 Modal.setAppElement('#root');
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-
+      
   function handleOpenNewTransactionModal() {
       setIsNewTransactionModalOpen(true);
   }
@@ -20,7 +21,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
 
@@ -30,6 +31,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
